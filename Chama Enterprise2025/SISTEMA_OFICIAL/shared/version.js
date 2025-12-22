4a6bd531550d5fcd823289848162f91956b2a61f
MisaelViana2015 <misael1215@gmail.com>
2025-12-22 19:55:24 -0300

    Cleanup: Remove redundant legacy and backup folders

--- Chama Enterprise2025/SISTEMA_OFICIAL/shared/version.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SYSTEM_VERSION = void 0;
-exports.SYSTEM_VERSION = "v1.2.0 - Fix Login Loop & Add Version Label";
--- Servidor-Teste/shared/version.ts
+++
@@ -1,1 +0,0 @@
-export const SYSTEM_VERSION = "v1.2.0 - Fix Login Loop & Add Version Label";
