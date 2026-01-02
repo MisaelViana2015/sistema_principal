120b437faab0e082cc7aa9291764fb41ddc9200e
MisaelViana2015 <misael1215@gmail.com>
2026-01-02 02:00:03 -0300

    style: Remove redundant AdminNavigation footer

--- SISTEMA_OFICIAL/client/src/components/AdminRoute.tsx
+++ SISTEMA_OFICIAL/client/src/components/AdminRoute.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import AdminNavigation from "../components/AdminNavigation";
 
 export default function AdminRoute() {
     const { user, isLoading } = useAuth();
@@ -18,12 +17,5 @@ export default function AdminRoute() {
         return <Navigate to="/garagem" replace />;
     }
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <div className="flex-1 pb-20"> {/* Padding bottom for fixed nav */}
-                <Outlet />
-            </div>
-            <AdminNavigation />
-        </div>
-    );
+    return <Outlet />;
 }
